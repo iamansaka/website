b6a6d9f336ec71bbb3107c5891e3ad781d3e1d61
ansaka <amahamoudou@dawan.fr>
2022-12-05 16:27:51 +0100

    Style page projet

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -3,31 +3,19 @@ import React from "react";
 import Link from "next/link";
 
 // Styled
-import { StyledButton } from "../../styles/module/Button.styled";
+import {StyledButton} from "../../styles/module/Button.styled";
 
 function Button({ children, url }) {
   return (
-    <Link href={url}>
       <StyledButton>
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="icon icon-tabler icon-tabler-arrow-down-right"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="#fff"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <line x1="7" y1="7" x2="17" y2="17" />
-            <polyline points="17 8 17 17 8 17" />
-          </svg>
-        </span>
-        {children}
+          <Link href={url}>
+              <a className="button_link">
+                  <div className="button_outline"></div>
+                  <div className="button_text">{children}</div>
+                  <img src="/images/arrow.svg" alt="icon fleche"/>
+              </a>
+          </Link>
       </StyledButton>
-    </Link>
   );
 }
 
--- pages/work/[slug].js
+++ pages/work/[slug].js
@@ -1,6 +1,14 @@
+// Librairies
 import React from "react";
-import { useRouter } from "next/router";
-import StyledContainer from "../../styles/base/Container.styled";
+import Link from "next/link";
+import {useRouter} from "next/router";
+
+// Component
+import Button from "../../components/Button/Button";
+
+// Styled
+import StyledContainer, {ContainerSmallStyled} from "../../styles/base/Container.styled";
+import {IntroStyled,LinkStyled, ListItemStyled, ThumbnailImageStyled, TitleStyled, WrapperStyled} from "../../styles/pages/ProjetShow.styled";
 
 export default function Projet() {
   const router = useRouter();
@@ -15,11 +23,40 @@ export default function Projet() {
   console.log("========================");
 
   return (
-    <>
-      <StyledContainer>
-        <a>Retour</a>
-        <h1>Projet 1</h1>
-      </StyledContainer>
-    </>
+      <>
+        <StyledContainer>
+          <IntroStyled>
+            <LinkStyled>
+              <Link href="/">
+                <a><strong>&#8592;</strong>Retour</a>
+              </Link>
+            </LinkStyled>
+            <TitleStyled>4 Pattes des poils et 1 coeur</TitleStyled>
+            <ListItemStyled>
+              <li><strong>Client</strong><br/>4PP1C</li>
+              <li><strong>Catégorie</strong><br/>Développement web</li>
+              <li><strong>Date</strong><br/>2021</li>
+            </ListItemStyled>
+          </IntroStyled>
+        </StyledContainer>
+        <ThumbnailImageStyled/>
+        <StyledContainer>
+          <WrapperStyled>
+            <h3>A propos du projet</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt auctor ante. Integer sodales aliquam interdum. Etiam ut porta augue.
+              Nam egestas neque eros, nec sodales massa bibendum ut. Donec vel imperdiet augue, vitae rhoncus mauris.
+            </p>
+            <p>
+              In non eleifend dui, at scelerisque felis. Praesent eros eros, auctor sit amet mollis eu, interdum vitae arcu. Donec gravida sit amet odio eget molestie. Donec consectetur sit amet ligula sed tempus. Fusce dictum nec velit ut tincidunt.
+            </p>
+            <div>
+              <Button url="/">
+                Voir le site web
+              </Button>
+            </div>
+          </WrapperStyled>
+        </StyledContainer>
+      </>
   );
 }
